e68de2817e5c80ff45d47f5da3b1bd64d803d8d2
vikasyadav-5253 <v.yadav3182005@gmail.com>
2025-05-26 15:51:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function joinNow() {
+  alert("Sign-up form coming soon!");
+}
+
+function bookTrial() {
+  alert("Free trial form coming soon!");
+}
+
+function getTrainer() {
+  alert("Trainer request form coming soon!");
+}
